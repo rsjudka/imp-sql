02398a78e522fb79c0496178ed18fffbb981a3fd
Robert Judka <robertjudka@gmail.com>
2017-11-05 17:36:07 -0600

    selecting correct tuples as defined by query3; not necessarily optimal solution, but not that bad

--- query.java
+++ query.java
@@ -31,6 +31,18 @@ public class query
                             "WHERE\tproduct.productid = soldvia.productid;"); 
         System.out.println("\nQuery2 result:");
         query2();
+
+        System.out.println("\n----------------------------------------------------------------------\n");
+
+        System.out.println("Running Query3:");
+        System.out.println("SELECT\t*\n" +
+                            "FROM\tproduct\n" +
+                            "WHERE\tNOT EXISTS\n" +
+                            "\t(SELECT 1\n" +
+                            "\tFROM soldvia\n" +
+                            "\tWHERE product.productid = soldvia.productid;"); 
+        System.out.println("\nQuery3 result:");
+        query3();
     }
 
     public static void readProduct() throws IOException
@@ -113,6 +125,22 @@ public class query
             {
                 String key = s.getproductID();
                 System.out.printf("%-6s       %-10s     %-4s            %s\n", key, pTable.get(key).getname(), pTable.get(key).getprice(), s.gettid());
+            }
+        }
+    }
+
+    public static void query3()
+    {
+        System.out.printf("%s    %s    %s    %s    %s\n", "ProductID", "ProductName", "ProductPrice", "VendorID", "CategoryID");
+        for (Map.Entry<String,pAttributes> entry : pTable.entrySet())
+        {
+            String key = entry.getKey();
+            pAttributes value = entry.getValue();
+            //ugly hack to check if key from sTable is in key from pTable, wasted memory cleaned up by gc
+                //O(lgn) runtime vs O(n) runtime for .contains()
+            if (Collections.binarySearch(sTable, new sAttributes(key, null, 0), new sComp()) < 0)
+            {
+                System.out.printf("%-6s       %-10s     %s              %s          %s\n",key, value.getname(), value.getprice(), value.getvendorID(), value.getcategoryID());
             }
         }
     }
